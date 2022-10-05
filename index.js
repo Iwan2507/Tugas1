1ec1d28d6cd140437aaec771a3060dcffd710f9b
iwan2507 <irwansyahsyah271@gmail.com>
2022-10-05 23:02:20 +0700

    perubahan 6

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const router = require('express').Router()
 const app = express();
-const port = 8080;
+const port = process.env.PORT ||8080;
 
 app.get("/", (req, res)=> res.send("Nama : Irwansyah"));
 app.get("/", (req, res)=> res.send("Kelas TI-4B"));
-app.get("/Makan"(req, res)=> res.send("Udah Makan?"));
+app.get("/Makan",(req, res)=> res.send("Udah Makan?"));
 
 app.listen(port, ()=>console.log('Aplikasi Berjalan di port ${port}'));
